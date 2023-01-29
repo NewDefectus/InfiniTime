f0856eca328b7034252c777cea84015165631305
Itay Sharon <itay.sharon100@gmail.com>
2023-01-30 00:21:46 +0200

    Refactor ANCS + Some fixes

--- src/components/ble/AppleNotificationCenterServiceClient.cpp
+++ src/components/ble/AppleNotificationCenterServiceClient.cpp
@@ -7,6 +7,19 @@
 
 using namespace Pinetime::Controllers;
 
+//#define _NOTIFDBG
+#ifdef _NOTIFDBG
+#define NOTIF_LOG(...) do \
+{ \
+  NotificationManager::Notification _notif{}; \
+  _notif.size = MIN(snprintf(_notif.message.data(), NotificationManager::MessageSize, __VA_ARGS__), NotificationManager::MessageSize); \
+  _notif.category = NotificationManager::Categories::SimpleAlert; \
+  notificationManager.Push(std::move(_notif)); \
+} while(0)
+#else
+#define NOTIF_LOG(...) {}
+#endif
+
 constexpr ble_uuid128_t AppleNotificationCenterServiceClient::ancsUuid;
 constexpr ble_uuid128_t AppleNotificationCenterServiceClient::ancsSourceCharUuid;
 constexpr ble_uuid128_t AppleNotificationCenterServiceClient::ancsControlCharUuid;
@@ -14,6 +27,10 @@ constexpr ble_uuid128_t AppleNotificationCenterServiceClient::ancsDataCharUuid;
 constexpr ble_uuid16_t  AppleNotificationCenterServiceClient::cccdUuid;
 
 constexpr uint16_t TitleLength = 30;
+constexpr uint16_t SubtitleLength = 50;
+constexpr uint16_t MessageLength = NotificationManager::MessageSize - TitleLength - SubtitleLength - 2; // -2 because of null byte for
+constexpr uint16_t MaxBufferSize = MAX(TitleLength, MAX(SubtitleLength, MessageLength));
+// others
 
 namespace {
   int OnDiscoveryEventCallback(uint16_t conn_handle, const struct ble_gatt_error* error, const struct ble_gatt_svc* service, void* arg) {
@@ -155,15 +172,12 @@ int AppleNotificationCenterServiceClient::OnDescriptorDiscoveryEventCallback(uin
       NRF_LOG_INFO("ANCS Descriptor discovered : %d", descriptor->handle);
       if (ancsSourceCCCDHandle == 0 && ancsSourceHandle == characteristicValueHandle) {
         ancsSourceCCCDHandle = descriptor->handle;
-//        NOTIF_LOG("Source CCCD");
         isSourceFound = true;
         isDescriptorFound = true;
       } else if (ancsDataCCCDHandle == 0 && ancsDataHandle == characteristicValueHandle) {
         ancsDataCCCDHandle = descriptor->handle;
         isDataFound = true;
         isDescriptorFound = true;
-//        NOTIF_LOG("Data CCCD");
-//        ble_gattc_write_flat(connectionHandle, ancsDataCCCDHandle, value, sizeof(value), OnANCSSubscribe, this);
       }
     }
   } else {
@@ -190,25 +204,73 @@ int AppleNotificationCenterServiceClient::OnDescriptorDiscoveryEventCallback(uin
   return 0;
 }
 
-void AppleNotificationCenterServiceClient::NewDataPacket(ble_gap_event* event) {
-
+void AppleNotificationCenterServiceClient::NewDataPacket(size_t * off) {
+  *off = sizeof(uint8_t) + // command
+         sizeof (uint32_t); // uid, ignore for now
+    
+  messageOffset = 0;
+  titleLength = 0;
+  subtitleLength = 0;
+  messageLength = 0;
+  currentNotif = NotificationManager::Notification{};
 }
 
-void AppleNotificationCenterServiceClient::ContinueDataPacket(ble_gap_event* event) {
-  char data[NotificationManager::MessageSize - TitleLength];
-  int availableToRead = MIN(OS_MBUF_PKTLEN(event->notify_rx.om), dataSizeLeft);
-  // Read as much as we can
-  if(os_mbuf_copydata(event->notify_rx.om, 0,  availableToRead, &data) == -1) return ;
-  snprintf(currentNotif.message.data() + messageOffset, availableToRead + 1, "%s", data);
+bool AppleNotificationCenterServiceClient::ParseHeader(ble_gap_event* event, size_t * off) {
+  if (off == nullptr) {
+    return false;
+}
   
-  // Mark as read
-  dataSizeLeft -= availableToRead;
+  uint8_t fieldId = 0;
+  if(os_mbuf_copydata(event->notify_rx.om, static_cast<int>(*off), sizeof(uint8_t), &fieldId) == -1) {
+    return false;
+  }
+  *off += sizeof(uint8_t);
+
+  uint16_t fieldLen = 0;
+  if(os_mbuf_copydata(event->notify_rx.om, static_cast<int>(*off), sizeof(uint16_t), &fieldLen) == -1) {
+    return false;
+  }
+  *off += sizeof(uint16_t);
+  switch (static_cast<NotificationAttributeID>(fieldId)) {
+    case NotificationAttributeID::NotificationAttributeIDTitle:
+      currentState = ANCSNotificationState::StartedTitle;
+      titleLength = MIN(fieldLen, TitleLength);
+      NOTIF_LOG("Title case - %d, %d = MIN(%d, %d)", fieldId, titleLength, fieldLen, TitleLength);
+      break;
+    case NotificationAttributeID::NotificationAttributeIDSubtitle:
+      currentState = ANCSNotificationState::StartedSubtitle;
+      subtitleLength = MIN(fieldLen, SubtitleLength);
+      NOTIF_LOG("Sub case - %d, %d = MIN(%d, %d)", fieldId, subtitleLength, fieldLen, SubtitleLength);
+      break;
+    case NotificationAttributeID::NotificationAttributeIDMessage:
+      currentState = ANCSNotificationState::StartedMessage;
+      messageLength = MIN(fieldLen, MessageLength);
+      NOTIF_LOG("Message case - %d, %d = MIN(%d, %d)", fieldId, messageLength, fieldLen, MessageLength);
+      break;
+    default:
+      NOTIF_LOG("Default case - %d, %d", fieldId, fieldLen);
+      break;
+  }
+  return true;
+}
+bool AppleNotificationCenterServiceClient::HandleFieldData(ble_gap_event* event, size_t * off, uint16_t * length) {
+  char data[MaxBufferSize + 1];
+  
+  if (off == nullptr || length == nullptr)
+    return false;
+  
+  // Try to read max packet size (from current position), but no more than the field's length
+  size_t availableToRead = MIN((OS_MBUF_PKTLEN(event->notify_rx.om) - *off), *length);
+  
+  if (os_mbuf_copydata(event->notify_rx.om, static_cast<int>(*off), static_cast<int>(availableToRead), &data) == -1) {
+    return false;
+  }
+  *off += availableToRead;
+  *length -= availableToRead;
+  (void)snprintf(currentNotif.message.data() + messageOffset, availableToRead + 1, "%s", data);
   messageOffset += availableToRead;
   
-  if (dataSizeLeft == 0)
-  {
-    EndDataPacket();
-  }
+  return true;
 }
 
 void AppleNotificationCenterServiceClient::EndDataPacket() {
@@ -218,9 +280,11 @@ void AppleNotificationCenterServiceClient::EndDataPacket() {
   notificationManager.Push(std::move(currentNotif));
   // If this crashes it means you have too many friends, AAT
   systemTask.PushMessage(System::Messages::OnNewNotification);
-  isEventInProgress = false;
-  dataSizeLeft = 0;
+  titleLength = 0;
+  subtitleLength = 0;
+  messageLength = 0;
   messageOffset = 0;
+  currentState = ANCSNotificationState::Idle;
 }
 
 void AppleNotificationCenterServiceClient::OnNotification(ble_gap_event* event) {
@@ -250,26 +314,27 @@ void AppleNotificationCenterServiceClient::OnNotification(ble_gap_event* event) 
       return ;
     }
 
-    // Enqueue the packet for future use
-//    notificationStack[packet.notificationUid] = packet;
-    
     // Request from iOS more data
+    if (currentState != ANCSNotificationState::Idle) {
+      EndDataPacket();
+    }
+    
     CPCommandBuilder builder{};
     builder.push(CommandID::CommandIDGetNotificationAttributes);
     builder.push(packet.notificationUid);
     builder.push(NotificationAttributeID::NotificationAttributeIDTitle);
-    builder.push(htons(TitleLength)); // max is 100, 25 for title (inc \0), 75 for content, at least for now
+    builder.push((TitleLength)); // max is 100, 25 for title (inc \0), 75 for content, at least for now
+//    builder.push(NotificationAttributeID::NotificationAttributeIDSubtitle);
+//    builder.push((SubtitleLength));
     builder.push(NotificationAttributeID::NotificationAttributeIDMessage);
-    builder.push(htons(NotificationManager::MessageSize - TitleLength));
+    builder.push((MessageLength));
     
-    isEventInProgress = false;
     ble_gattc_write_flat(event->notify_rx.conn_handle, ancsControlHandle, builder.data(), builder.size(), nullptr, nullptr);
-    
-    
-    // TODO:
+    currentState = ANCSNotificationState::RequestedInfo;
     // 1. Fix instagram notifications messing up others (?)
     // 2. Support hebrew (V)
-    // 3. Cleanup code (maybe check uid for failsafe?)
+    // 2.5. emoji support?
+    // 3. Cleanup code (maybe check uid for failsafe?) (V?)
     // 4. Handle calls
     // 5. Battery
     // 6. Music
@@ -282,66 +347,64 @@ void AppleNotificationCenterServiceClient::OnNotification(ble_gap_event* event) 
 //      systemTask.PushMessage(System::Messages::OnNewCall);
 //    }
   } else if (event->notify_rx.attr_handle == ancsDataHandle) {
-    if (dataSizeLeft != 0)
-    {
-      ContinueDataPacket(event);
-      // If we reset the event (asking for a new packet), then we should end early
-      if (!isEventInProgress)
-      {
-        EndDataPacket();
+    size_t packetLen = OS_MBUF_PKTLEN(event->notify_rx.om);
+    size_t offset = 0;
+    while (packetLen > offset) {
+      switch (currentState) {
+        case ANCSNotificationState::Idle:
+          return ;
+        case ANCSNotificationState::RequestedInfo:
+          NewDataPacket(&offset);
+          currentState = ANCSNotificationState::FinishedField;
+          break;
+        case ANCSNotificationState::StartedTitle:
+          if (!HandleFieldData(event, &offset, &titleLength)) {
+            return ;
+          }
+          if (0 != titleLength) {
+            NOTIF_LOG("Need more data for title - %d", titleLength);
+            break ;
+          }
+          currentState = ANCSNotificationState::FinishedField;
+          messageOffset += 1;
+          break ;
+        case ANCSNotificationState::FinishedField:
+          // TODO(user): edge case when ID and length are split between 2 packets
+          if(!ParseHeader(event, &offset)) {
+            NOTIF_LOG("Failed to parse header at %d", offset);
+            return ;
+          }
+          break;
+        case ANCSNotificationState::PreSubtitle:
+          if (subtitleLength == 0) {
+            currentState = ANCSNotificationState::FinishedField;
+            break ;
+          }
+          currentState = ANCSNotificationState::StartedSubtitle;
+          // FALLTHROUGH
+        case ANCSNotificationState::StartedSubtitle:
+          if (!HandleFieldData(event, &offset, &subtitleLength)) {
+            return ;
+          }
+          if (0 != subtitleLength) {
+            break ;
+          }
+          currentState = ANCSNotificationState::FinishedField;
+          *(currentNotif.message.data() + messageOffset) = '\n';
+          messageOffset += 1;
+          break ;
+        case ANCSNotificationState::StartedMessage:
+          if (!HandleFieldData(event, &offset, &messageLength)) {
+            return ;
+          }
+          if (0 != messageLength) {
+            NOTIF_LOG("Need more data for message - %d", messageLength);
+            break ;
+          }
+          EndDataPacket();
+          currentState = ANCSNotificationState::Idle;
+          return ;
       }
-      return ;
-    }
-    
-    int off = sizeof(uint8_t) + // command
-              sizeof (uint32_t) + // uid, ignore for now
-              sizeof (uint8_t); // Messy for now, skipping IDTitle check TODO
-    
-    messageOffset = 0;
-    dataSizeLeft = 0;
-    currentNotif = NotificationManager::Notification{};
-    isEventInProgress = true;
-    
-    // TODO(user): If the response is larger than the negotiated GATT Maximum Transmission Unit (MTU), it is split into multiple fragments by the NP.
-    //  The NC must recompose the response by splicing each fragment. The response is complete when the complete tuples for each
-    //  requested attribute has been received.
-    uint16_t titleLen = 0;
-    if(os_mbuf_copydata(event->notify_rx.om, off, sizeof(uint16_t), &titleLen) == -1) return ;
-    off += sizeof (uint16_t);
-    
-    if (titleLen != 0)
-    {
-      char data[TitleLength + 1];
-      titleLen = MIN(titleLen, TitleLength);
-      if(os_mbuf_copydata(event->notify_rx.om, off, titleLen, &data) == -1) return ;
-      off += titleLen;
-      
-      snprintf(currentNotif.message.data() + messageOffset, titleLen + 1, "%s", data);
-      messageOffset += titleLen + 1;
-    }
-    
-    off += sizeof (uint8_t); // Skip check for text attr id
-    uint16_t messageLen = 0;
-    if(os_mbuf_copydata(event->notify_rx.om, off, sizeof(uint16_t), &messageLen) == -1) return ;
-    off += sizeof (uint16_t);
-    if (messageLen != 0)
-    {
-      char data[NotificationManager::MessageSize - TitleLength];
-      
-      int availableToRead = MIN(OS_MBUF_PKTLEN(event->notify_rx.om) - off, NotificationManager::MessageSize - TitleLength);
-      if(os_mbuf_copydata(event->notify_rx.om, off,  availableToRead, &data) == -1) return ;
-      
-      messageLen = MIN(messageLen, NotificationManager::MessageSize - TitleLength);
-      
-      snprintf(currentNotif.message.data() + messageOffset, availableToRead + 1, "%s", data);
-      messageOffset += availableToRead;
-      
-      dataSizeLeft = messageLen - availableToRead;
-    }
-
-    if (dataSizeLeft == 0)
-    {
-      EndDataPacket();
     }
   }
 }
@@ -352,7 +415,9 @@ void AppleNotificationCenterServiceClient::Reset() {
   ancsSourceHandle = 0;
   ancsControlHandle = 0;
   ancsDataHandle = 0;
-  dataSizeLeft = 0;
+  titleLength = 0;
+  subtitleLength = 0;
+  messageLength = 0;
   ancsSourceCCCDHandle = 0;
   ancsDataCCCDHandle = 0;
   isDiscovered = false;
@@ -363,11 +428,13 @@ void AppleNotificationCenterServiceClient::Reset() {
   isReady = false;
   isSourceReady = false;
   isDataReady = false;
+  currentState = ANCSNotificationState::Idle;
   notificationStack.clear();
 }
 
 void AppleNotificationCenterServiceClient::Discover(uint16_t connectionHandle, std::function<void(uint16_t)> onServiceDiscovered) {
   NRF_LOG_INFO("[ANCS] Starting discovery");
+  NOTIF_LOG("[ANCS] Starting discovery");
   this->onServiceDiscovered = onServiceDiscovered;
   ble_gattc_disc_svc_by_uuid(connectionHandle, &ancsUuid.u, OnDiscoveryEventCallback, this);
 }
